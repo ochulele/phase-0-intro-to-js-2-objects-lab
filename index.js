5335446c8639542f2b78c8df528fc223c79e23aa
ochuk <ochuciano@gmail.com>
2023-03-01 01:18:57 -0500

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Ochuciano",
+    streetAddress: "1 Pennstation"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmp = {...employee};
+    delete newEmp[key];
+    return newEmp;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
